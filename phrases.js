c797e275c2e9d3ede31d4d78b3f83947ed4ce6a5
Jorge Yau <codenameyau@gmail.com>
2018-08-10 09:05:01 -0400

    Add chinese phrases

---
+++ phrases.js
@@ -0,0 +1,12 @@
+module.exports = [
+  {
+    en: "I'll come home next week.",
+    zh: "Wǒ xià zhōu huí jiā",
+    zh_sim: "我下周回家",
+  },
+  {
+    en: "I'm going to work today.",
+    zh: "Wǒ jīntiān yào qù shàngbān",
+    zh_sim: "我今天要去上班"
+  },
+];
