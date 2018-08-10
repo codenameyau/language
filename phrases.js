4cbc57e1191efc867d81120ad162fbc6ff37d8f9
Jorge Yau <codenameyau@gmail.com>
2018-08-10 09:10:54 -0400

    Add japanese phrase

--- phrases.js
+++ phrases.js
@@ -2,11 +2,11 @@ module.exports = [
   {
     en: "I'll come home next week.",
     zh: "Wǒ xià zhōu huí jiā",
-    zh_sim: "我下周回家",
+    ja: "Raishū kaeru yo",
   },
   {
     en: "I'm going to work today.",
     zh: "Wǒ jīntiān yào qù shàngbān",
-    zh_sim: "我今天要去上班"
+    ja: "Watashi wa kyō hataraku tsumoridesu",
   },
 ];
