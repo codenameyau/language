914ca99cb3afea3aba8330bbc5ffc6638e5db32f
Jorge Yau <codenameyau@gmail.com>
2018-08-11 22:39:25 -0400

    Add translate script

--- README.md
+++ README.md
@@ -2,8 +2,9 @@
 ### I'll come home next week
 - **Chinese:** Wǒ xià zhōu huí jiā
 - **Japanese:** Raishū kaeru yo
-- **Spanish:** volveré a casa la próxima semana
+- **Spanish:** Volveré a casa la próxima semana
 
 ### I'm going to work today
 - **Chinese:** Wǒ jīntiān yào qù shàngbān
 - **Japanese:** Watashi wa kyō hataraku tsumoridesu
+- **Spanish:** voy a trabajar hoy
--- phrases.js
+++ phrases.js
@@ -3,11 +3,12 @@ module.exports = [
     en: "I'll come home next week",
     zh: "Wǒ xià zhōu huí jiā",
     ja: "Raishū kaeru yo",
-    es: "volveré a casa la próxima semana"
+    es: "Volveré a casa la próxima semana"
   },
   {
     en: "I'm going to work today",
     zh: "Wǒ jīntiān yào qù shàngbān",
     ja: "Watashi wa kyō hataraku tsumoridesu",
+    es: "voy a trabajar hoy",
   },
 ];
