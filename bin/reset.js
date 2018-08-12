f40540b84819b2fd41acbe607b314f980e9a106d
Jorge Yau <codenameyau@gmail.com>
2018-08-12 16:53:24 -0400

    Add reset

---
+++ bin/reset.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+const phrases = require('../phrases');
+const PHRASES_PATH = path.resolve(__dirname, '/../phrases.json');
+
+const main = () => {
+  const emptyPhrases = {
+    ...phrases,
+    phrases: []
+  };
+
+  const stream = fs.createWriteStream(PHRASES_PATH);
+  stream.write(JSON.stringify(emptyPhrases, null, 2));
+};
+
+main();
