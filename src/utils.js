b00de5356bd199b15063138c4e4d7f70bb74490c
Jorge Yau <codenameyau@gmail.com>
2018-08-28 22:43:03 -0400

    Add shuffle util

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,15 @@
-exports.randomInt = (min, max) => {
+const randomInt = exports.randomInt = (min, max) => {
   return Math.min(Math.floor(Math.random() * max), max);
 };
 
-exports.shuffle = (array) => {
-
+const shuffle = exports.shuffle = (array) => {
+  const shuffledArray = [...array];
+  shuffledArray.forEach((item, i) => {
+    const randomIndex = randomInt(i, shuffledArray.length);
+    const currentValue = shuffledArray[i];
+    const selectedValue = shuffledArray[randomIndex];
+    shuffledArray[i] = selectedValue;
+    shuffledArray[randomIndex] = currentValue;
+  });
+  return shuffledArray;
 };
