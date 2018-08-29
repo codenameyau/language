af745c983b066220a95c5f843fa5ed674f749e7f
Jorge Yau <codenameyau@gmail.com>
2018-08-28 23:38:28 -0400

    Add WIP study program

--- bin/study.js
+++ bin/study.js
@@ -1,7 +1,8 @@
-const phrases = require('../phrases.json');
+const readline = require('readline');
+const phrases = require('../phrases.json').phrases;
 const randomInt = require('../src/utils').randomInt;
 const romanize = require('../src/romanize').romanize;
-const languages = require('../languages');
+const shuffle = require('../src/utils').shuffle;
 
 const match = (phrase, answer) => {
   return (
@@ -10,15 +11,49 @@ const match = (phrase, answer) => {
   )
 };
 
-const study = exports.study = () => {
-  const length = phrases.phrases.length;
-  const selection = randomInt(0, length);
-  const phrase = phrases.phrases[selection];
-  console.log(phrase.en);
+const study = exports.study = (count = 1) => {
+  const shuffledPhrases = shuffle(phrases);
+  const reader = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  const phrasePromise = async (phrase, language) => {
+    return new Promise((resolve, reject) => {
+      const question = `  ${phrase.en}\n  (${language}) > `;
+      reader.clearLine();
+      reader.question(question, (answer) => {
+        resolve(answer);
+      });
+    });
+  };
+
+  const promises = shuffledPhrases.slice(0, count).map((phrase) => {
+    return phrasePromise(phrase, 'zh');
+  });
+
+  console.log(promises);
+
+  // const phrase = shuffledPhrases[0];
+  // const promise = phrasePromise(phrase, 'zh');
+
+  // for (let i=0; i<count; i++) {
+  //   const phrase = shuffledPhrases[i];
+  //   const languages = Object.keys(phrase).filter((lang) => lang !== 'en');
+  //   console.log(`  ${phrase.en}\n`);
+  //   languages.forEach((language) => {
+  //     const question = `  (${language}) > `;
+  //     reader.question(question, (answer) => {
+  //       console.log(answer);
+  //     });
+  //   });
+  // }
+
 };
 
 const main = () => {
-  study();
+  const count = Math.floor(process.argv[2]) || 1;
+  study(count);
 };
 
 require.main === module && main();
