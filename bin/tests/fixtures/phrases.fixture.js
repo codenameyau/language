dc4e41a916f3ce57d6c18bb45af9f9bb7690c526
Jorge Yau <codenameyau@gmail.com>
2019-03-30 11:14:28 -0400

    Add study test

---
+++ bin/tests/fixtures/phrases.fixture.js
@@ -0,0 +1,70 @@
+export const phrases = {
+  "phrases": [
+    {
+      "en": "How much is it?",
+      "zh": "Duōshǎo qián?",
+      "ja": "Ikuradesu ka?",
+      "es": "¿Cuánto cuesta?"
+    },
+    {
+      "en": "When is your birthday",
+      "zh": "Nǐ de shēngrì shì shénme shíhòu",
+      "ja": "Anata no tanjōbi wa itsudesu ka",
+      "es": "Cuando es tu cumpleaños"
+    },
+    {
+      "en": "Let's go to a restaurant tonight",
+      "zh": "Wǒmen jīn wǎn qù cānguǎn ba",
+      "ja": "Kon'ya resutoran ni ikimashou",
+      "es": "Vamos a un restaurante esta noche"
+    },
+    {
+      "en": "Let's go to the grocery store",
+      "zh": "Wǒmen qù záhuò diàn ba",
+      "ja": "Shokuryōhin-ten ni ikimashou",
+      "es": "Vamos a la tienda de comestibles"
+    },
+    {
+      "en": "Can you go to buy food",
+      "zh": "Nǐ néng qù mǎi cài ma?",
+      "ja": "Tabemono o kau koto ga dekimasu ka?",
+      "es": "¿Puedes ir a comprar comida"
+    },
+    {
+      "en": "When are you coming home",
+      "zh": "Nǐ shénme shíhòu huí jiā",
+      "ja": "Anata wa itsu-ka ni kaerimasu ka",
+      "es": "Cuando vendrás a casa"
+    },
+    {
+      "en": "I'm going to work today",
+      "zh": "Wǒ jīntiān yào qù shàngbān",
+      "ja": "Watashi wa kyō hataraku tsumoridesu",
+      "es": "Voy a trabajar hoy"
+    },
+    {
+      "en": "I'm going home",
+      "zh": "Wǒ yào huí jiāle",
+      "ja": "Watashi wa ie ni iku yo",
+      "es": "Me voy a casa"
+    },
+    {
+      "en": "I'm going home now",
+      "zh": "Wǒ zhèngzài huí jiā",
+      "ja": "Mō kaeru ne",
+      "es": "Voy a casa ahora"
+    },
+    {
+      "en": "I'm going home tomorrow",
+      "zh": "Wǒ míngtiān yào huí jiāle",
+      "ja": "Ashita ienikaeru yo",
+      "es": "Me voy a casa mañana"
+    },
+    {
+      "en": "I'll come home next week",
+      "zh": "Wǒ xià zhōu huí jiā",
+      "ja": "Raishū kaeru yo",
+      "es": "Volveré a casa la próxima semana"
+    }
+  ]
+}
